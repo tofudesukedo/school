61c77a9fcf447ebea584c0d2bdd581aaba0eb6d7
Toru Takeuchi <tofudesukedo@gmail.com>
2024-05-11 15:28:39 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Toru's crazy Japanese lessons</h1>
+  <h1>English lessons</h1>
+  <a href="http://tofudesukedo.github.io/dist"
+    >特定商取引法に基づく表記用ページ</a
+  >
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
